3c313c9fbf6f379cd89a11fddd7e2ec973d1de54
Viktor Hronec <zamr666@gmail.com>
2017-02-14 23:34:27 +0100

    removed unnecessary functions

--- clippy/clippygen.py
+++ clippy/clippygen.py
@@ -6,43 +6,12 @@ import os
 CLIPPY_TOP = Image.open(os.path.join(os.path.dirname(__file__), 'top.png'))
 CLIPPY_MID = Image.open(os.path.join(os.path.dirname(__file__), 'mid.png'))
 CLIPPY_LOW = Image.open(os.path.join(os.path.dirname(__file__), 'low.png'))
-
 TOP_H = CLIPPY_TOP.size[1]
 MID_H = CLIPPY_MID.size[1]
 LOW_H = CLIPPY_LOW.size[1]
 LINE_H = 65
-
-# line_count = 0
-# current_height = 0
-# line_height = 35
 FONT_PATH = os.path.join(os.path.dirname(__file__), 'COURIER.TTF')
 FONT = ImageFont.truetype(FONT_PATH, size=30, encoding="utf-8")
-# i = Image.new('RGB', (555, 1000))
-# draw = ImageDraw.Draw(i)
-
-
-def add_top(im: Image):
-    im.paste(CLIPPY_TOP, (0, 0))
-    return im
-
-
-def add_line(im: Image, height):
-    im.paste(CLIPPY_MID, (0, height))
-    return im
-
-
-def add_bottom(im: Image, height):
-    im.paste(CLIPPY_LOW, (0, height))
-    return im
-
-
-def drawline(draw: ImageDraw.Draw, text, height):
-    draw.text((30, height), text, (0, 0, 0), font=FONT)
-
-
-def cut_bottom(im: Image, height):
-    crop = im.crop((0, 0, 555, height))
-    return crop
 
 
 def get_img(text: str):
@@ -51,24 +20,24 @@ def get_img(text: str):
     i = Image.new('RGB', (555, 1000))
     draw = ImageDraw.Draw(i)
 
-    i = add_top(i)
+    i.paste(CLIPPY_TOP, (0, 0))
+
     current_height += TOP_H
     line_count = 1
 
     text_lines = textwrap.wrap(text, 28)
     for line in text_lines:
-        i = add_line(i, current_height)
+        i.paste(CLIPPY_MID, (0, current_height))
         line_count += 1
         current_height += MID_H
-
-        drawline(draw, line, line_height)
+        draw.text((30, line_height), line, (0, 0, 0), font=FONT)
         line_height += LINE_H
 
-    i = add_bottom(i, current_height)
+    i.paste(CLIPPY_LOW, (0, current_height))
     current_height += LOW_H
-    i = cut_bottom(i, current_height)
+    crop = i.crop((0, 0, 555, current_height))
 
-    return i
+    return crop
 
 if __name__ == '__main__':
     input_text = input("Insert the text: ")
